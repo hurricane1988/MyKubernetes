847a58a36acc157739449d0ea18f6664680d993b
admin <hurricane1988@163.com>
2022-02-02 14:07:33 +0800

    add get pods

--- pkg/deploy/deployment.go
+++ pkg/deploy/deployment.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -66,4 +68,51 @@ func GetDeployFromNamespace() {
 		DeploymentSlice = append(DeploymentSlice, string(results02))
 	}
 	fmt.Println(DeploymentSlice)
+}
+
+// CreateDeployment 创建deployment方法
+func CreateDeployment() {
+	config, err := clientcmd.BuildConfigFromFlags("", configs.KubeconfigPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	client, _ := kubernetes.NewForConfig(config)
+
+	deploymentClient := client.AppsV1().Deployments(apiv1.NamespaceDefault)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx-test",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "nginx",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "web",
+							Image: "nginx:1.12",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http-80",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
