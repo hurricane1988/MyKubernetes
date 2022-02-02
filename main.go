55a7c58c71fa90e8448531eca55fc1cedf546d59
admin <hurricane1988@163.com>
2022-02-02 11:55:39 +0800

    add get pods

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"log"
-	"os"
+	"context"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	a := os.Getegid()
-	log.Fatalf("当前系统版本信息: %s\n", a)
+	config, err := clientcmd.BuildConfigFromFlags("", "/Users/admin/.kube/config")
+	if err != nil {
+		panic(err)
+	}
+	client, _ := kubernetes.NewForConfig(config)
+	pods, err := client.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range pods.Items {
+		fmt.Printf(" 命名空间是：%v\n pod名字：%v\n IP：%v\n\n", v.Namespace, v.Name, v.Status.PodIP)
+	}
 }
